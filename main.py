25ba03e5d9eef0557aceb248a92dea7c258b8832
zakr1 <zakr1995@gmail.com>
2025-09-16 20:27:32 +0300

    DONE! 1. Creating an application for shortening links. 2. Storing data in json 3. Endpoint for receiving data from the form 4. Endpoint that redirects to the saved link by identifier

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 import string
 from random import choice
-from typing import Annotated
+from typing import Annotated, Optional
 import os
 
 import aiofiles
 import json
 from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 if os.path.isdir("static"):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+
+def render_template(template_name: str, request: Request, short_url_code: Optional[str] = None):
+    return templates.TemplateResponse(template_name, {"request": request, "short_url_code": short_url_code})
 
 
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="mainpage.html")
+@app.get("/", response_class=HTMLResponse, name="root")
+async def root(request: Request, short_url: Optional[str] = None):
+    return render_template('mainpage.html', request, short_url_code=short_url)
 
 
-@app.get("/{short_url}")
-async def short_url_handler(short_url: str):
+@app.get("/{short_code}", name="redirect_to_url")
+async def short_url_handler(short_code: str):
     async with aiofiles.open("urls.JSON", mode="r") as f:
         urls_data = json.loads(await f.read())
-    longurl = urls_data.get(short_url)
+    longurl = urls_data.get(short_code)
+    if not longurl:
+        return RedirectResponse(url="/", status_code=307)
     return RedirectResponse(longurl)
 
 
-@app.post("/")
-async def create_url(longurl: Annotated[str, Form()]):
+@app.post("/", name="shorten")
+async def create_url(request: Request, longurl: Annotated[str, Form()]):
     alphabet = string.ascii_letters + string.digits
     short_url = ''.join(choice(alphabet) for _ in range(6))
 
@@ -45,4 +50,7 @@ async def create_url(longurl: Annotated[str, Form()]):
 
     async with aiofiles.open("urls.JSON", mode="w") as f:
         await f.write(json.dumps(urls_data, indent=4))
-    return {"short_url": short_url, "longurl": longurl}
+
+    redirect_url = str(request.url_for("root").include_query_params(short_url=short_url))
+
+    return RedirectResponse(url=redirect_url, status_code=303)
