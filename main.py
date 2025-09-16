c298ad89bb21c456c81103cc936f589b5d0a0b74
zakr1 <zakr1995@gmail.com>
2025-09-16 17:17:11 +0300

    fix code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+import os
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -6,17 +7,22 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+if os.path.isdir("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="mainpage.html")
 
 
 @app.get("/{short_url}")
 async def short_url_handler(short_url: str):
     return {"message": f"Hello {short_url}"}
+
 
 @app.post("/login/")
 async def create_url(longurl: Annotated[str, Form()]):
